7e600f3de0d24e7e46c46384a9f60ca0d88672d5
Hukumka <hukumka212@gmail.com>
2024-03-18 20:50:32 +0700

    Fix build, migrate to cargo2nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs@{ self, nixpkgs, flake-parts, ...}: 
@@ -13,24 +13,35 @@
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = {self', pkgs, system, ...}:
         let
-          rustVersion = "1.65.0";
+          rustVersion = "1.67.0";
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [inputs.cargo2nix.overlays.default (import inputs.rust-overlay)];
+          };
           rustPkgs = pkgs.rustBuilder.makePackageSet {
             inherit rustVersion;
-            packageFun = ./Cargo.nix;
+            packageFun = import ./Cargo.nix;
+
+            packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "yeslogic-fontconfig-sys";
+                overrideAttrs = drv: {
+                  propagatedNativeBuildInputs = drv.propagatedNativeBuildInputs or [ ] ++ [
+                    pkgs.fontconfig.dev
+                  ];
+                };
+              })
+            ];
           };
         in {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ inputs.cargo2nix.overlays.default (import inputs.rust-overlay) ];
-          };
           packages = rec {
-            spellcard_generator = (rustPkgs.workscape.spellcard_generator {});
+            spellcard_generator = (rustPkgs.workspace.spellcard_generator {}).bin;
             default = spellcard_generator;
           };
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
               pkg-config
-              fontconfig
+              fontconfig.dev
               rust-analyzer-unwrapped
               rust-bin.stable.${rustVersion}.default
             ];
